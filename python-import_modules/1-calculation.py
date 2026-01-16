2fa48cbb60c769fb6074ebcada1f96ea2106d67e
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-16 11:51:21 +0100

    fix: correct main execution conditional statement syntax

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,9 +3,9 @@
 
 from calculator_1 import add, sub, mul, div
 
-__name__ == "__main__":
-    a = 10
-    b = 5
+__name__ == "__main__"
+a = 10
+b = 5
 
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} + {} = {}".format(a, b, sub(a, b)))
