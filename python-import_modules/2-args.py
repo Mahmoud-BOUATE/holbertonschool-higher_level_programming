9850778dacc8396868fbcf5e60ae94bda3cdf364
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-16 12:34:09 +0100

    add

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-
-if len(argv) > 1:
-    print(f"{len(argv) - 1} arguments:")
+if __name__ == "__main__":
+    if len(argv) > 1:
+        print(f"{len(argv) - 1} arguments:")
     for i in range(1, len(argv)):
         print(f"{i}: {argv[i]}")
 else:
