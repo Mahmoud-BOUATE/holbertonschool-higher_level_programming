6edfdb3a577cdc34ef61d64f1276ae8b66d5e3f1
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-16 12:36:31 +0100

    correction code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-    if len(argv) > 1:
-        print(f"{len(argv) - 1} arguments:")
-    for i in range(1, len(argv)):
-        print(f"{i}: {argv[i]}")
-else:
-    print("0 arguments.")
+    nb = len(argv) - 1
+    if nb > 0:
+        if nb == 1:
+            print(f"{nb} argument:")
+        else:
+            print(f"{nb} arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
+    else:
+        print("0 arguments.")
