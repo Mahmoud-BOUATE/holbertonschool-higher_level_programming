bcb28a87bbf262361b2cfc77948e414cb682d160
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-19 17:55:15 +0100

    feat: implement print_matrix_integer function for matrix printing

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for element in line:
+            print("{:d}".format(element), end=" ")
+        print()
+    
