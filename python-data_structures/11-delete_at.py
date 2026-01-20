a675dfcb897409c9d4c99c060d13650f2aeb2730
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-20 12:26:13 +0100

    test

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 
 def delete_at(my_list=[], idx=0):
     new_list = my_list
-
-    del (new_list[idx])
-    return new_list
-    if idx < 0:
+    if idx < 0 and idx > len(my_list):
         return my_list
+    else:
+        del (new_list[idx])
+        return new_list
