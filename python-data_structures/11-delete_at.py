eeba3ab1f0e4186fb38910bb966f557de34a3307
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-20 12:29:38 +0100

    correction de code

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    new_list = my_list.copy()
-    if 0 <= idx <= len(my_list) - 1:
-
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        del (new_list[idx])
-        return new_list
 
+    new_list = my_list.copy()
+    del new_list[idx]
+    return new_list
