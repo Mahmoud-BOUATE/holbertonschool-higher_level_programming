82a36aa212f5727fc58c7dc0f31c6f352905dd56
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-21 12:20:16 +0100

    correction of the return value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for key in a_dictionary:
-        max_valeur = max(a_dictionary.values())
-        return max_valeur
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
