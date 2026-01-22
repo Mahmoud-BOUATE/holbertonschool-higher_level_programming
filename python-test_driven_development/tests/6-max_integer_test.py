6a4bdee84b98970d0298968c7dc5c1ab71d65e85
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-22 15:43:49 +0100

    add max integer test function

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer function
+"""
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer"""
+
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-10, 0, 5, 3]), 5)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_no_argument(self):
+        self.assertIsNone(max_integer())
+
+    def test_same_numbers(self):
+        self.assertEqual(max_integer([2, 2, 2]), 2)
+
+    def test_large_numbers(self):
+        self.assertEqual(max_integer([1000, 5000, 2000]), 5000)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([1.5, 2.7, 0.3]), 2.7)
