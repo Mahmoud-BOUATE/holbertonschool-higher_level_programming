df12607e63beaac33807861d80c225e9e0e22f4e
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-26 14:40:38 +0100

    add arguments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
 """
-this module define a class Square
+This module defines a class Square.
 """
 
 
 class Square:
+    """Represents a square with a private size attribute."""
+
     def __init__(self, __size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            __size (int): The size of the square (private attribute).
+        """
+        # __size is intended to be a private attribute of the square
         pass
