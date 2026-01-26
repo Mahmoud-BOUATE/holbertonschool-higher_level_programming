5fcbf4221efe4d880c454aeb778d79aae25960ff
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-26 15:52:47 +0100

    test

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""
-This module defines a class Square.
-"""
-
 
 class Square:
-    """Represents a square with a private size attribute."""
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a new Square instance.
-
-        Args:
-            size (int): The size of the square.
-        """
-        if type(size) is not int:
+        """Initialize a new Square."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Retourne l'aire du carré"""
-
+        """Return the area of the square."""
         return self.__size * self.__size
