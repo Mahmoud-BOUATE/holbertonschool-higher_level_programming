4fe349effa4ae272d381461fb618f60f1706e97f
mahmoud BOUATE <12065@holbertonschool.com>
2026-01-27 13:15:53 +0100

    and task 0 and main script

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,11 +55,11 @@ class Square:
             return
 
         # Décalage vertical (position[1])
-        for _ in range(self.__position[1]):
+        for j in range(self.__position[1]):
             print()
 
         # Boucle pour chaque ligne du carré
-        for _ in range(self.__size):
+        for i in range(self.__size):
             # Décalage horizontal (position[0])
             print(" " * self.__position[0], end="")
             # Les # du carré
---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class rectangle.
+"""
+
+
+class rectangle:
+    """Represents an empty rectangle."""
+    pass
