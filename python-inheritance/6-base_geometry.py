fdd8a378da1771ab830d0fbf63f5dc7a73e7e806
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-04 13:27:56 +0100

    add task 6 and main script

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines a BaseGeometry class
+"""
+
+class BaseGeometry:
+    """Base class for geometry"""
+
+    def area(self):
+        """Raises an exception because area is not implemented"""
+        raise Exception("area() is not implemented")
