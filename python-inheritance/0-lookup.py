d948e89f51311bed4f4e79b60e162fd5d6535971
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-04 11:43:30 +0100

    delete a blank line at end of file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module lookup: retourne les attributs d'un objet."""
 
+
 def lookup(obj):
     """Retourne la liste des attributs et méthodes d'un objet."""
     return dir(obj)
