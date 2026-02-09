e50d5c61b9346f42b7e53924dd7a7366f8a54826
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-09 14:50:11 +0100

    add task 9 and main script

---
+++ python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Student module
+Defines a Student class with JSON serialization capability
+"""
+
+
+class Student:
+    """Initialize a Student instance"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return a dictionary representation of the instance"""
+
+        return self.__dict_
