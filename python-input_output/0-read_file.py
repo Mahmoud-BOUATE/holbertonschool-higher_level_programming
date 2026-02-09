14c356edd005fcf3b52200f19c90bbd20db60291
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-09 10:54:13 +0100

    add tasl 0  and main script

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""ouvir un fichier en mode lecture en utilisant with"""
+
+
+def read_file(filename="my_file_0.txt"):
+    """Cette fonction indique le fichier à ouvrir et le mode d'ouverture"""
+
+    with open("my_file_0.txt", 'r', encoding="utf-8") as f:
+        contenu = f.read()
+    print(contenu)
