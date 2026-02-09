f4b5ad223efefdb538b6b9c3d146861fc9cc1887
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-09 11:18:26 +0100

    test

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ouvir un fichier en mode lecture en utilisant with"""
 
-def write_file(filename="my_file_0.txt", text="This School is so cool!\n"):
+
+def write_file(filename="", text=""):
     """Cette fonction indique le fichier ou va écrir et le mode d'ouverture"""
 
     with open(filename, 'w', encoding="utf-8") as f:
-        return len(text)
+            f.write(text)
+            return len(text)
