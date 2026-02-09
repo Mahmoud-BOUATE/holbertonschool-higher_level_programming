c83b8dc50e4cdc7bf9a70d028ea60d67465df57d
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-09 11:19:18 +0100

    fix indentation in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ def write_file(filename="", text=""):
     """Cette fonction indique le fichier ou va écrir et le mode d'ouverture"""
 
     with open(filename, 'w', encoding="utf-8") as f:
-            f.write(text)
-            return len(text)
+        f.write(text)
+        return len(text)
