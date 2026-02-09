8be6e8ce6a91fe9916a625961620ed746ffd94b8
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-09 11:00:55 +0100

    test

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename="my_file_0.txt"):
 
     with open(filename, 'r', encoding="utf-8") as f:
         simple_line = f.read()
-    print(simple_line)
+    print(simple_line, end="")
