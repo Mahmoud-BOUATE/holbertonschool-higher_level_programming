247e5be8a202e6f35a886264bb622af5992bb05c
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-11 11:41:58 +0100

    test

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -7,8 +7,9 @@ Docstring for python-serialization.task_01_pickle
 
 import pickle
 
+
 class CustomObject:
-    def __init__(self,name, age, is_student):
+    def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
