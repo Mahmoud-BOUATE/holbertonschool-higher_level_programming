ee0e14a42d627a8430a3f140e9f929cb372a32c0
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-11 10:56:52 +0100

    add commentaire

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,10 +5,12 @@ Docstring for python-serialization.task_00_basic_serialization
 import json
 
 def serialize_and_save_to_file(data, filename):
+    """fonction de serialisation"""
     filename = json.dumps(data)
     print(filename)
 
 
 def load_and_deserialize(filename):
-    filename = json.loads(filename)
+    """fonction de déserialisation"""
+    data = json.loads(filename)
     print(filename)
