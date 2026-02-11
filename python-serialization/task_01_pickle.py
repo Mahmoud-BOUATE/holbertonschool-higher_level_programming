9a284d394458b3755fd5f7e328849de3c6065033
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-11 11:45:55 +0100

    test

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
-
-"""
-Docstring for python-serialization.task_01_pickle
-"""
-
-
 import pickle
 
 
 class CustomObject:
     def __init__(self, name, age, is_student):
-        """Class qui contiet le nome, age , is_student"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """fonction qui affiche les attributs"""
+        """Affiche les attributs de l'objet au format demandé"""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """fonction de serialisation"""
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
+        """Sérialise l'objet courant dans le fichier filename"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            # En cas d'erreur (permissions, etc.), on ne fait rien
+            return None
 
     @classmethod
     def deserialize(cls, filename):
-        """fonction de déserialisation"""
-        with open(filename, "rb") as f:
-            obj = pickle.load(f)
-        return obj
+        """Charge et retourne un objet CustomObject depuis filename"""
+        try:
+            with open(filename, "rb") as f:
+                obj = pickle.load(f)
+                if isinstance(obj, cls):
+                    return obj
+                return None
+        except Exception:
+            # Fichier inexistant ou mal formé
+            return None
