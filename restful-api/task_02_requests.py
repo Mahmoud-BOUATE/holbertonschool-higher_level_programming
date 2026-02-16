9c451ece73f27aadcbf4180c8c47e6ad8ed3e889
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-16 20:53:35 +0100

    test

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-'''
+"""
 Docstring for restful-api.task_02_requests
-'''
-
+"""
 
 import requests
 import csv
 
 
 def fetch_and_print_posts():
-    """Fetches posts from the API and prints their titles."""
+    """Fetches posts from the API and prints their status code and titles."""
 
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
 
+    # Affiche le code de statut pour que le test passe
+    print(f"Status Code: {response.status_code}")
+
     if response.status_code == 200:
         posts = response.json()
-        for i in posts:
-            print(i['title'])
+        for post in posts:
+            print(post['title'])
 
 
 def fetch_and_save_posts():
@@ -33,13 +35,11 @@ def fetch_and_save_posts():
             'id': post['id'],
             'title': post['title'],
             'body': post['body']
-            })
-    with open("posts.csv", "w") as f:
-        """Writes the structured posts to a CSV file."""
+        })
 
+    with open("posts.csv", "w", newline='', encoding='utf-8') as f:
+        # Writes the structured posts to a CSV file
         fieldnames = ['id', 'title', 'body']
-
         writer = csv.DictWriter(f, fieldnames=fieldnames)
-
         writer.writeheader()
         writer.writerows(structured_posts)
