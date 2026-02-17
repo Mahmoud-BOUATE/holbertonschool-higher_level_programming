3b85c0456a1c4e874873e5c8544360cd24bd0f83
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-17 12:07:10 +0100

    fix: standardize response message for /status endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,7 +34,7 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"ok")
+            self.wfile.write(b"OK")
 
         else:
             self.send_response(200)
