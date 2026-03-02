7c46396ca744692847465d52639b7f422d7484d3
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-02 16:04:26 +0100

    feat: update SQL script for full creation of second_table with data insertion

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,13 @@
--- Full creation
+-- Création de la table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
+
+-- Insertion des données
+INSERT INTO second_table (id, name, score) VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
