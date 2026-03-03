45153e3d064f217561721c167d0dbf5b01ad3f2b
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-03 09:32:51 +0100

    fix: add comment for granting all privileges in user creation SQL script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Root user
 CREATE USER IF NOT EXISTS 'user_0d_1`@`localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all privileges on the MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost';
