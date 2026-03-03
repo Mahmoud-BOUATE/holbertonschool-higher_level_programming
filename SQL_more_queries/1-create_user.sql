1039d7cf931ec56be51f16b0b819bb7c03773157
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-03 09:33:25 +0100

    fix: add WITH GRANT OPTION to user privileges in SQL script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Root user
 CREATE USER IF NOT EXISTS 'user_0d_1`@`localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges on the MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost'
+WITH GRANT OPTION;
