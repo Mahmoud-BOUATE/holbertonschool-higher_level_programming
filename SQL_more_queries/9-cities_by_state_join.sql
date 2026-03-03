76a487f7685726fd505b51a7cb09bd9dc4ac2bac
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-03 12:16:15 +0100

    feat: add SQL query to list all cities with their states

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- List all cities with their states
+SELECT cities.id,
+       cities.name AS city,
+       states.name AS state
+FROM cities
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
