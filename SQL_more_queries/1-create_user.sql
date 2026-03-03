8e5d5d0d2f5fecd53ccd2e6068f0b27443f61906
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-03 09:33:55 +0100

    fix: correct syntax and improve comments in user creation SQL script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- Root user
-CREATE USER IF NOT EXISTS 'user_0d_1`@`localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Create user user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
 -- Grant all privileges on the MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1`@`localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 WITH GRANT OPTION;
+
+-- Reload privilege tables
+FLUSH PRIVILEGES;
