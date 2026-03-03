380314faeb68fd393b04c5a40c13fab40137c2ab
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-03 12:18:39 +0100

    fix: simplify SQL query by removing unnecessary aliases

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 -- List all cities with their states
-SELECT cities.id,
-       cities.name AS city,
-       states.name AS state
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states
 ON cities.state_id = states.id
