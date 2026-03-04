1baf0588d9aa4247674f72219f981e93ac5ac862
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 15:12:12 +0100

    feat: add script to display states matching a given name from the database

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY states.id ASC"
+    ).format(state_name)
+    cursor.execute(query, (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
