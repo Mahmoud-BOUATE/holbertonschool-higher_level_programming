ff683299ca5af004488bce669df8d97cddf7761c
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 15:47:17 +0100

    feat: add State class and SQLAlchemy Base instance for states table

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a State class and a Base instance using SQLAlchemy ORM
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create Base instance
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that links to the states table"""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
