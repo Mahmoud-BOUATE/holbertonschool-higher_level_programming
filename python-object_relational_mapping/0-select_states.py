018f1d5d2954c80778f956a7d2a664d5e56d98a6
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 14:05:15 +0100

    feat: add initial database setup and state listing script

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from a MySQL database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create cursor
+    cursor = db.cursor()
+
+    # Execute query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all results
+    rows = cursor.fetchall()
+
+    # Print results
+    for row in rows:
+        print(row)
+
+    # Close connection
+    cursor.close()
+    db.close()
