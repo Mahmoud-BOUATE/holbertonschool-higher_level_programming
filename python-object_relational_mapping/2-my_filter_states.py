81c5583935f142f89957d75ce1ccd3e619cec16e
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 14:52:19 +0100

    fix: modify SQL query to use BINARY comparison for case-sensitive state name matching in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 	cursor = db.cursor()
 	query = (
 		"SELECT * FROM states "
-		"WHERE name = '{}' "
+		"WHERE name BINARY = '{}' "
 		"ORDER BY states.id ASC"
 	).format(state_name)
 	cursor.execute(query)
