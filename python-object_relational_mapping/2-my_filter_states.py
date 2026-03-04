48e0719e07240e2fa097dfb6857799ef735eddb7
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 14:58:01 +0100

    fix: correct indentation for improved readability in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,12 +10,12 @@ import sys
 
 
 if __name__ == "__main__":
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
-	state_name = sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-	db = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
@@ -23,16 +23,16 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-	cursor = db.cursor()
-	query = (
-		"SELECT * FROM states "
-		"WHERE BINARY name  = '{}' "
-		"ORDER BY states.id ASC"
-	).format(state_name)
-	cursor.execute(query)
+    cursor = db.cursor()
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name  = '{}' "
+        "ORDER BY states.id ASC"
+    ).format(state_name)
+    cursor.execute(query)
 
-	for row in cursor.fetchall():
-		print(row)
+    for row in cursor.fetchall():
+        print(row)
 
-	cursor.close()
-	db.close()
+    cursor.close()
+    db.close()
