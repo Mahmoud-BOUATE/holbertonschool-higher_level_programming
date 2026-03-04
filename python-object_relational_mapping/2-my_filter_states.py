a3b3c15bacc84fb183dbaef7f6111c4d8737319f
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-04 14:55:09 +0100

    fix: correct indentation for database connection parameters in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
 	state_name = sys.argv[4]
 
 	db = MySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=username,
-		passwd=password,
-		db=database
-	)
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
 	cursor = db.cursor()
 	query = (
