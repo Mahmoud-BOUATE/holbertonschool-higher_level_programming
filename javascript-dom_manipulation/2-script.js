8e8016cd582cf97aa94874621a4858574a35142e
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-24 13:49:23 +0100

    feat: add HTML structure and JavaScript for header color change on click

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,9 @@
+
+const redHeader = document.querySelector('#red_header');
+const header = document.querySelector('header');
+
+
+redHeader.addEventListener('click', function () {
+    header.classList.add("red");
+});
+
