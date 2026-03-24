0b13e1fd38899ce45c78f966fd5a8898270b7d6c
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-24 15:29:44 +0100

    feat: add initial HTML structure and JavaScript for header update functionality

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,9 @@
+const newHeader = document.getElementById("update_header");
+const header = document.querySelector('header');
+
+newHeader.addEventListener("click", function() {
+    header.innerText = "New Header!!!";
+});
+
+
+
